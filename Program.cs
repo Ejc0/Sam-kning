9be2b8eaff86c0640ea140d62d9d57933193efb0
Johan Hentze <60313056+Ejc0@users.noreply.github.com>
2022-11-23 13:10:19 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Samåkning
+{
+    internal class Program
+    {
+        
+
+        static void Main(string[] args)
+        {
+            double avstånd, drivmedelspris; // drivmedelskostnad Liter och Mil
+            double milförbrukning;
+            double kördamil, maxmil, tidigaremil; //service kostnad
+            double livslängd, nypris; //däcklivslängd
+            double kostnad, pris, servicepris;
+            
+            
+            avstånd = 8.8; drivmedelspris = 27; milförbrukning = 0.65;
+
+            kördamil = 10; maxmil = 150; tidigaremil = 100; servicepris = 5000;
+
+            livslängd = 4250; nypris = 5000;
+            double driftkostnad = avstånd * drivmedelspris * milförbrukning;
+            double servicekostnad = (kördamil / maxmil) * servicepris;
+            double däckkostnad = (kördamil / livslängd) * nypris;
+            double totalkostnad = driftkostnad + servicekostnad + däckkostnad;
+            totalkostnad = totalkostnad / 2;
+            Console.WriteLine(avstånd * drivmedelspris * milförbrukning);
+            Console.WriteLine((kördamil/maxmil)* servicepris);
+            Console.WriteLine((kördamil/livslängd)*nypris);
+            Console.WriteLine(totalkostnad);
+            Console.WriteLine("Hello, World!");
+            
+            
+        }
+    }
+}
